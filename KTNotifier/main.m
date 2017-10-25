c99aed0f16bad8ee2782b0c4ac95801e6bd22ecc
ShenHaoWen <805154269@qq.com>
2017-10-25 16:38:01 +0800

    Initial Commit

---
+++ KTNotifier/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  KTNotifier
+//
+//  Created by Karthus on 2017/10/25.
+//  Copyright © 2017年 karthus. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
